b147192d6031154093d57e422cce1c52bba987c5
Mariya Ilina <m.polibit@gmail.com>
2025-03-24 14:20:45 +0300

    feat: emplement getNextFridayThe13th

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -210,8 +210,15 @@ function getWeekNumberByDate(date) {
  * Date(2024, 0, 13) => Date(2024, 8, 13)
  * Date(2023, 1, 1) => Date(2023, 9, 13)
  */
-function getNextFridayThe13th(/* date */) {
-  throw new Error('Not implemented');
+function getNextFridayThe13th(date) {
+  const dateForFind = new Date(date);
+  dateForFind.setUTCDate(13);
+
+  while (dateForFind.getUTCDay() !== 5) {
+    dateForFind.setUTCMonth(dateForFind.getUTCMonth() + 1);
+  }
+  dateForFind.setHours(-24);
+  return dateForFind;
 }
 
 /**
